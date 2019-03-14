371497207b9d260ddd6a88f75d29408c76961485
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-03-14 11:52:23 +0300

    Refactoring task4 in react Events and States

--- src/App.js
+++ src/App.js
@@ -6,18 +6,55 @@ import authors from "./data";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
+import AuthorDetail from "./AuthorDetail";
 
 class App extends Component {
+  state = {
+    currentAuthor: null,
+    filterAuthors: authors
+  };
+
+  selectAuthor = author => {
+    this.setState({ currentAuthor: author });
+  };
+
+  unselectAuthor = () => {
+    this.setState({ currentAuthor: null, filterAuthors: authors });
+  };
+
+  getView = () => {
+    if (this.state.currentAuthor) {
+      return <AuthorDetail authorDetail={this.state.currentAuthor} />;
+    } else {
+      return (
+        <AuthorsList
+          authors={this.state.filterAuthors}
+          selectAuthor={this.selectAuthor}
+          filterAuthors={this.filterAuthors}
+        />
+      );
+    }
+  };
+
+  filterAuthors = authorFilter => {
+    this.setState({
+      filterAuthors: authors.filter(author => {
+        const authorName = `${author.first_name} ${
+          author.last_name
+        }`.toLowerCase();
+        return authorName.includes(authorFilter.toLowerCase());
+      })
+    });
+  };
+
   render() {
     return (
       <div id="app" className="container-fluid">
         <div className="row">
           <div className="col-2">
-            <Sidebar />
+            <Sidebar unselectAuthor={this.unselectAuthor} />
           </div>
-          <div className="content col-10">
-            <AuthorsList authors={authors} />
-          </div>
+          <div className="content col-10">{this.getView()}</div>
         </div>
       </div>
     );
